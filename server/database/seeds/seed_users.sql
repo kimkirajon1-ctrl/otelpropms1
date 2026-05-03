846cc66b72935087c9bd6c549843c604bbf23744
kimkirajon1-ctrl <kimkirajon1@gmail.com>
2026-05-03 17:25:23 +0300

    Create seed_users.sql

---
+++ server/database/seeds/seed_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, full_name, email, role) VALUES
+('admin', '$2a$10$X8mYIqTqUvI/q9tU6M.pEe9mB6r5vX/5JqR4G.uX.Vz1q2w3e4r5t', 'Sistem Yöneticisi', 'admin@hotelpms.com', 'ADMIN'),
+('recep1', '$2a$10$X8mYIqTqUvI/q9tU6M.pEe9mB6r5vX/5JqR4G.uX.Vz1q2w3e4r5t', 'Resepsiyon Görevlisi', 'frontoffice@hotelpms.com', 'FRONT_OFFICE'),
+('kat1', '$2a$10$X8mYIqTqUvI/q9tU6M.pEe9mB6r5vX/5JqR4G.uX.Vz1q2w3e4r5t', 'Kat Hizmetleri Şefi', 'housekeeping@hotelpms.com', 'HOUSEKEEPING');
