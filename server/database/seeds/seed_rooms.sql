bac5ebd0e332771f585101021f123393201f09d8
kimkirajon1-ctrl <kimkirajon1@gmail.com>
2026-05-03 17:25:47 +0300

    Create seed_rooms.sql

---
+++ server/database/seeds/seed_rooms.sql
@@ -0,0 +1,6 @@
+INSERT INTO rooms (room_number, room_type, price_per_night, floor, status, description) VALUES
+('101', 'Single', 1500.00, 1, 'AVAILABLE', 'Bahçe manzaralı tek kişilik oda'),
+('102', 'Double', 2500.00, 1, 'AVAILABLE', 'Deniz manzaralı çift kişilik oda'),
+('201', 'Suite', 5000.00, 2, 'AVAILABLE', 'Jakuzili lüks suit'),
+('202', 'Double', 2200.00, 2, 'DIRTY', 'Çıkış yapılmış, temizlik bekliyor'),
+('301', 'Family', 3500.00, 3, 'MAINTENANCE', 'Klima bakımı yapılıyor');
