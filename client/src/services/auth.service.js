1cd1bf2fb16fc7afa438322d9009b58b092b32d3
kimkirajon1-ctrl <kimkirajon1@gmail.com>
2026-05-03 18:43:00 +0300

    Create auth.service.js

---
+++ client/src/services/auth.service.js
@@ -0,0 +1,18 @@
+import api from './api';
+
+const authService = {
+  login: async (credentials) => {
+    const response = await api.post('/auth/login', credentials);
+    return response.data;
+  },
+  getMe: async () => {
+    const response = await api.get('/auth/me');
+    return response.data;
+  },
+  logout: () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+};
+
+export default authService;
